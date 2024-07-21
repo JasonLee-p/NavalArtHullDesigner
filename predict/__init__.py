276b547467b4b878fd000c142910f89993d755a9
JasonLee-p <2593292614@qq.com>
2024-07-21 09:01:45 +0800

    更新init

--- predict/__init__.py
+++ predict/__init__.py
@@ -1,4 +1,4 @@
 """
 对点位进行预测（使用非线性回归，或者机器学习）
 """
-from mlp_predictor import *
+from .mlp_predictor import *
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,14 +1,21 @@
 """
 测试
 """
-import unittest
+from .test_APIClient import *
+from .test_ShipPaint import *
+from .test_ShipRead import *
+from .test_main_editor import *
+from .test_main_logger import *
+from .test_cv2replacement import TestCV2Replacements
+from .test_funcs_utils import TestFuncsUtils
 
 
-def run_test():
+def run_test() -> bool:
     loader = unittest.TestLoader()
     suite = loader.discover(start_dir='.', pattern='test_*.py')
     runner = unittest.TextTestRunner()
     runner.run(suite)
+    return True
 
 
 if __name__ == '__main__':
